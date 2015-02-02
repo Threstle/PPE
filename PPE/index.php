3c6f5a0b13e60b88dff5217a7e155dded685aa41
Threstle <Etienne.Chaumont@gmail.com>
2015-02-02 18:01:35 +0100

    first

---
+++ PPE/index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+
+"http://www.w3.org/TR/html4/loose.dtd">
+
+
+
+<html>
+
+<head>
+  
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" media="screen and (max-width: 600px)" href="css/styleMob.css">
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,500,400italic,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
+  <script type="text/javascript" src="js/script.js"></script>
+  <script src="js/parallax-master/parallax-master/deploy/jquery.parallax.js"></script>
+
+</head>
+
+<body>
+	<header>
+	</header>
+	<content>
+		<div id="contact"></div>
+	<!-- 	<ul id="dustBack">
+			<li class='layer dust1' data-depth='0.80'></li>
+
+			<li class='layer dust1' data-depth='0.20'></li>
+			<li class='layer dust1' data-depth='0.50'></li>
+		</ul> -->
+		
+		
+		<div id="leftSide" class="leftPush">
+			<ul id="app">
+				<li data="<div id='section1' class='section'><h1>Timbre</h1><img class='headPhoto' src='img/timbre2.png'><h2><span class='important'>Timbre</span> is an application for <span class='important'>Ipad and Iphone</span> conceived and developed in the context of a school project on the theme of <span class='important'>augmented knowledge</span>. It's a new way to <span class='important'>communicate</span> via the creation of <span class='important'>sound landscapes</span>.</h2></img></div>"><img src="img/timbre2.png"></img>
+			</ul>
+			<ul id="exp">
+				<li data="<h1>Phéromones</h1>"><img src='img/pheromone.png'></img></li>
+				<li data="<h1>Landscapes to Stare in the Dark</h1>"><img src='img/ltsitd2.png'></img></li>
+			</ul>
+			<ul id="sit">
+				
+			</ul>
+			<ul id="jeu">
+				<li data="<h1>Zéphir</h1>"><img src='img/zephir.png'></img></li>
+				<li data="<h1>Russian Planet</h1>"><img src='img/russian.png'></img></li>
+			</ul>
+
+
+
+		</div>
+		<div id="floatingMenu" class="leftPush">
+			<div id="selectorLeft" class="selector"></div>
+			<div id="selectorRight" class="selector"></div>
+			<div id="hideButton"><span></span></div>
+			
+			
+			<div id="insideMenu">
+			
+			<div id="logo"></div>
+			<div id="menuTexte">
+				<h1>ETIENNE CHAUMONT</h1>
+				<ul>
+					<li class="menuElement" data="app"><a>APPLICATIONS</a></li>
+					<li class="menuElement" data="exp"><a>EXPERIENCES</a></li>
+					<li class="menuElement" data="sit"><a>SITES</a></li>
+					<li class="menuElement" data="jeu"><a>JEUX</a></li>
+				</ul>
+				<span id="separator"></span>
+				<a id="contactButton" data="<div id='contentRight'><div id='section1' class='section'><h1>About Me</h1><img id='contactPhoto' src='img/photo.png'><h2><b>Hello, my name is Etienne Chaumont, I'm a <span class='important'>multimedia developer</span> from France. I love <span class='important'>front-end</span> and <span class='important'>back-end</span> development, innovative and user-friendly interfaces, new ways of thinking, <span class='important'>game making</span> and, most of all, <span class='important'>burgers</span>.</b></h2><ul id='iconsAbout'><li><img src='img/iconOrdi.png'></img></li><li><img src='img/iconGame.png'></img></li><li><img src='img/iconBurger.png'></img></li></ul></div><div id='section2' class='section'><h1>My Skills</h1><ul id='skillsList'><li><h3>BACK-END DEV</h3><img src='img/iconGear.png'></img><ul id='back' class='skills'><span></span><li><a>PHP5</a></li><li><a>NodeJS</a></li><li><a>Java</a></li><li><a>Wordpress</a></li><li><a>MYSQL/PLSQL</a></li><li><a>Objective C</a></li></ul><img class='sousBadges' src='img/sousBadgeVert.png'></img></li><li><h3>FRONT-END DEV</h3><img src='img/iconFront.png'></img><ul id='front' class='skills'><span></span><li><a>HTML 5 & CSS 3</a></li><li><a>Javascript</a></li><li><a>JQuery</a></li><li><a>Three JS</a></li><li><a>Unity 3D</a></li><li><a>SEO</a></li></ul><img class='sousBadges' src='img/sousBadgeJaune.png'></img></li><li><h3>DESIGN</h3><img src='img/iconDesign.png'></img><ul id='design' class='skills'><span></span><li><a>Photoshop</a></li><li><a>Illustrator</a></li></ul><img class='sousBadges' src='img/sousBadgeRouge.png'></img></li></ul></div><div id='section3' class='section'><h1>Experience</h1><div class='dip'><div class='dateExp'>2014/2015</div><h2>CRMA - Gobelins</h2></div><div class='exp'><div class='dateExp'>2014</div><h2>Hatier apprenticeship</h2></div><div class='exp'><div class='dateExp'>2013</div><h2>Happy Neuron internship</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh augue, semper ut congue vitae, commodo ac tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In venenatis placerat massa eu auctor. Nunc non sodales urna. Curabitur commodo quam sit amet augue vestibulum volutpat. Quisque pellentesque vehicula vulputate. Nulla sit amet arcu id dolor cursus molestie. Morbi accumsan euismod lacus, ac mollis ipsum mollis at. Praesent sit amet mi ac urna hendrerit varius pellentesque convallis ante.</p></div><div class='dip'><div class='dateExp'>2012/2013</div><h2>MIAGE Licence</h2></div><div class='exp'><div class='dateExp'>2012</div><h2>Portsmouth University internship</h2></div><div class='dip'><div class='dateExp'>2010/2012</div><h2>DUT SRC</h2></div></div><div id='section4' class='section'><h1>Extra Infos</h1><h2>If you want to know <span class='important'>more</span> about me, feel free to mail me, add me on linkedin, facebook, or even follow me on twitter !</h2></div>">ABOUT ME</a>
+
+
+			</div>
+			</div>
+		</div>
+		<div id="rightSide" class="rightSidePush">
+			<div id="rightMenu" class="rightMenuPush">
+				<ul>
+					<li><a href="#section1">ABOUT ME</a></li>
+					<li><a href="#section2">MY SKILLS</a></li>
+					<li><a href="#section3">CONTACT ME</a></li>
+				</ul>
+			</div>
+			<span id="showButton"><span></span></span>
+			<div id="contentRight">
+				<div id="section1" class="section">
+				<h1>About Me</h1>
+				
+				<img id="contactPhoto" src="img/photo.png">
+				
+				<h2><b>Hello, my name is Etienne Chaumont, I'm a <span class="important">multimedia developer</span> from France. I love <span class="important">front-end</span> and <span class="important">back-end</span> development, innovative and user-friendly interfaces, new ways of thinking, <span class="important">game making</span> and, most of all,  <span class="important">burgers</span>.</b></h2>
+				<ul id="iconsAbout">
+					<li><img src="img/iconOrdi.png"></img></li>
+					<li><img src="img/iconGame.png"></img></li>
+					<li><img src="img/iconBurger.png"></img></li>
+				</ul>
+				</div>
+				<div id="section2" class="section">
+				<h1>My Skills</h1>
+					<ul id="skillsList">
+
+						<li><h3>BACK-END DEV</h3><img src="img/iconGear.png"></img><ul id="back" class="skills">
+																					<span></span>
+																					<li><a>PHP5</a></li>
+																					<li><a>NodeJS</a></li>
+																					<li><a>Java</a></li>
+																					<li><a>Wordpress</a></li>
+																					<li><a>MYSQL/PLSQL</a></li>
+																					<li><a>Objective C</a></li>
+																					</ul>
+																					<img class="sousBadges" src="img/sousBadgeVert.png"></img>
+																					</li>
+
+						<li><h3>FRONT-END DEV</h3><img src="img/iconFront.png"></img><ul id="front" class="skills">
+																						<span></span>
+																						<li><a>HTML 5 & CSS 3</a></li>
+																						<li><a>Javascript</a></li>
+																						<li><a>JQuery</a></li>
+																						<li><a>Three JS</a></li>
+																						<li><a>Unity 3D</a></li>
+																						<li><a>SEO</a></li>
+																						</ul>
+																						<img class="sousBadges" src="img/sousBadgeJaune.png"></img>
+																						</li>
+
+						<li><h3>DESIGN</h3><img src="img/iconDesign.png"></img><ul id="design" class="skills">
+																				<span></span>
+																				<li><a>Photoshop</a></li>
+																				<li><a>Illustrator</a></li>
+																				</ul>
+																				<img class="sousBadges" src="img/sousBadgeRouge.png"></img>
+																			</li>
+					</ul>
+				</div>
+				<span><img img="img/iconPara.png"></img></span>
+	
+				<div id="section3" class="section">
+				<h1>Extra Infos</h1>
+				<h2>If you want to know <span class="important">more</span> about me, feel free to mail me, add me on linkedin, facebook, or even follow me on twitter !</h2>
+					<ul id="linkSocial">
+						<li id="twi"><img src="img/iconTwitter.png"></img></li>
+						<li id="fac"><img src="img/iconFb.png"></img></li>
+						<li id="tum"><img src="img/iconTumblr.png"></img></li>
+						<li id="lin"><img src="img/iconLink.png"></img></li>
+					</ul>
+				</div>
+			
+				<!--<div id="corner">
+					<img src="img/corner.png"></img>
+					<a href="https://www.facebook.com/etienne.chaumont" target="_blank"><img id="fbLogo" class="socialLogo" src="img/logoFbVert.png" ></img></a>
+				</div>-->
+			</div>
+
+		</div>
+		<?php include 'metaLogo.html' ?>
+	</content>
+	<div id="loader"><h1>WAIT MOTHERFUCKER</h1></div>
+
+</body>
+
+</html>
